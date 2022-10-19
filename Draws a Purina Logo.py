a39ea348a59901444ebc7c8cf7182ca9bef48c0d
AhmadAlrafati <61342366+AhmadAlrafati@users.noreply.github.com>
2022-10-19 14:58:15 -0400

    Add files via upload

---
+++ Draws a Purina Logo.py
@@ -0,0 +1,47 @@
+#Name:  Ahmad Alrafati
+#Email: ahmad.alrafati96@myhunter.cuny.edu
+#Date: september 7, 2022
+#This program draws a purina logo.
+
+import turtle
+wn = turtle.Screen()
+lina = turtle.Turtle()
+lina.pensize(5)
+lina.shape("circle")
+
+lina.color("green")
+lina.forward(300)
+lina.right(90)
+lina.forward(100)
+lina.right(90)
+lina.forward(100)
+
+lina.right(90)
+lina.color("blue")
+lina.forward(300)
+lina.right(90)
+lina.forward(100)
+lina.right(90)
+lina.forward(100)
+
+lina.right(90)
+lina.color("cyan")
+lina.forward(300)
+lina.right(90)
+lina.forward(100)
+lina.right(90)
+lina.forward(100)
+
+lina.right(90)
+lina.color("red")
+lina.forward(300)
+lina.right(90)
+lina.forward(100)
+lina.right(90)
+lina.forward(100)
+
+
+
+wn.exitonclick()
+
+
