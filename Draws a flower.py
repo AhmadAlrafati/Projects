6a9a03d065188a980af61a9f331f46a75a37eb23
AhmadAlrafati <61342366+AhmadAlrafati@users.noreply.github.com>
2022-10-19 14:55:16 -0400

    Add files via upload

---
+++ Draws a flower.py
@@ -0,0 +1,14 @@
+#Name:  Ahmad Alrafati
+#Email: ahmad.alrafati96@myhunter.cuny.edu
+#Date: september 7, 2022
+#This program draws a flower
+import turtle
+t = turtle.Turtle()
+for i in range(9):
+    t.forward(100)
+    t.left(105)
+    t.forward(52)
+    t.left(105)
+    t.forward(100)
+    t.right(170)
+    
