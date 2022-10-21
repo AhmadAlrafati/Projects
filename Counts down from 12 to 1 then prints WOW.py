4ebab0e16ced74e99408a9f199cdb3521bf6f78f
AhmadAlrafati <61342366+AhmadAlrafati@users.noreply.github.com>
2022-10-21 00:24:36 -0400

    Add files via upload

---
+++ Counts down from 12 to 1 then prints WOW.py
@@ -0,0 +1,9 @@
+#Name:  Ahmad Alrafati
+#Email: ahmad.alrafati96@myhunter.cuny.edu
+#Date: september 10, 2022
+#This program counts down from 12 to 1 then prints WOW
+
+
+for i in range(12, 0, -1):
+    print(i)
+print("WOW")
---
+++ Prints Practice makes perfect a given amount of times.py
@@ -0,0 +1,9 @@
+#Name:  Ahmad Alrafati
+#Email: ahmad.alrafati96@myhunter.cuny.edu
+#Date: september 7, 2022
+#This program prints "Practice makes perfect" a given amount of times
+
+ahmad=int(input("Enter repetition time:"))
+
+for i in range(ahmad):
+    print("Practice makes perfect.")
